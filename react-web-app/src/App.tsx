6a611f14b3fc3393798c64b44801b0661376cf79
BalaKondaveeti <135067322+BalaKondaveeti@users.noreply.github.com>
2024-03-31 07:52:35 -0400

    Now uploading to s3

--- react-web-app/src/App.tsx
+++ react-web-app/src/App.tsx
@@ -2,36 +2,44 @@ import { useCallback, useState } from 'react'
 import FileSelector from './components/fileSelector'
 import SubmitButton from './components/submitButton'
 import CustomTextField from './components/textfield'
-import ReactS3Client from 'react-aws-s3-typescript';
+import AWS from 'aws-sdk';
 
-const s3Config = {
-  bucketName: 'filesaver-lamba-learn',
-  dirName: 'directory-name',      /* Optional */
-  region: 'ap-south-1',
-  accessKeyId:'ABCD12EFGH3IJ4KLMNO5',
-  secretAccessKey: 'a12bCde3f4+5GhIjKLm6nOpqr7stuVwxy8ZA9bC0',
-  s3Url: 'https:/your-aws-s3-bucket-url/'     /* Optional */
-}
-
+AWS.config.update({
+  region: 'us-east-1', // e.g., 'us-west-2'
+  accessKeyId:  'AKIA47CRVLP3OP56FQ6I',
+  secretAccessKey: '6CpktMq7jvlbmljOh1ZCYj3xD7Wc75njP3l0+6zB',
+});
 
 function App() {
 
   const [inputText, setInputText] = useState<string>('');
   const [selectedFile, setSelectedFile] = useState<File | null | undefined>();
-  const s3 = new ReactS3Client(s3Config);
 
-  const onsubmit = useCallback(async ()=>{
+  const onsubmit = useCallback(async () => {
     console.log(inputText);
     console.log(selectedFile?.name);
-    setInputText('');
-    setSelectedFile(null);
+    if (selectedFile) {
+      try {
+        const response = await new AWS.S3().upload({
+          Bucket: 'filesaver-lamba-learn', // Your S3 Bucket name
+          Key: selectedFile.name, // File name you want to save as in S3
+          Body: selectedFile,
+        }).promise();
+        console.log(response.toString() + "The output");
+        setInputText('');
+        setSelectedFile(null);
+      } catch (error) {
+        console.error('Upload failed:', error);
+        alert('Upload faiibgled, check the console for more information.');
+      }
+    }
   }, [inputText, selectedFile]);
 
   return (
     <>
-      <CustomTextField inputValue={inputText} setInputValue={setInputText}/>
-      <FileSelector setSelectedFile={setSelectedFile}/>
-      <SubmitButton onSubmit={onsubmit}/>
+      <CustomTextField inputValue={inputText} setInputValue={setInputText} />
+      <FileSelector setSelectedFile={setSelectedFile} />
+      <SubmitButton onSubmit={onsubmit} />
     </>
   )
 }
